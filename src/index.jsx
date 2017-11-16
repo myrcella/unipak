240ce75d1c19894d72dbd389ad15c00eafbb49c2
Anssi Mäkinen <anssi.a.makinen@gmail.com>
2017-11-16 15:28:34 +0200

    WIP: try out google-map-react

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HelloF, HelloF2, HelloC} from './Hello.jsx';
+import GoogleMap from 'google-map-react';
 
-const element = <div>
-    <HelloF name='Anssi' />
-    <HelloF2 name='Saga' />
-    <HelloC name='Kaarlo' />
+const SomeComponent = ({text}) => <button>{text}</button>;
+
+const element = <div style={{width: '100%', height: '100%'}}> 
+    <GoogleMap defaultCenter={{lat: 60.186, lng: 24.831}} defaultZoom={15}>
+        <SomeComponent lat={60.187} lng={24.832} text={"Text"} />
+    </GoogleMap>
 </div>;
 
 ReactDOM.render(element, document.getElementById('app'));
