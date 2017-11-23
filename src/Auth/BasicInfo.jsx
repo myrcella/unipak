afebc8e09850ab5f70b72c6c1445b1bcde07d514
Anssi Mäkinen <anssi.a.makinen@gmail.com>
2017-11-23 19:35:22 +0200

    Add form for basic user info
    Add helper component for form text fields with a label and help text.

--- src/Auth/BasicInfo.jsx
+++ src/Auth/BasicInfo.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Checkbox, Button } from 'react-bootstrap';
+import FieldGroup from '../util/FieldGroup';
 
 export default class BasicInfo extends React.Component {
   constructor(props) {
@@ -10,6 +12,42 @@ export default class BasicInfo extends React.Component {
     return (
       <form>
         TODO: Auth: basic info
+        <FieldGroup
+          id="formUsername"
+          type="text"
+          label="Username"
+          help="Your username will be visible to all users."
+        />
+        <FieldGroup
+          id="formRealname"
+          type="text"
+          label="Real name"
+          help="Your real name will only be visible to those users you interact with."
+        />
+        <FieldGroup
+          id="formEmail"
+          type="email"
+          label="E-mail"
+        />
+        <FieldGroup
+          id="formPhone"
+          type="tel"
+          label="Phone number"
+        />
+        <FieldGroup
+          id="formDOB"
+          type="date"
+          label="Date of birth"
+        />
+        <Checkbox>
+          I agree to Unipak Terms and Conditions
+        </Checkbox>
+        <Checkbox>
+          I wish to receive Unipak newsletter
+        </Checkbox>
+        <Button type="submit">
+          Next
+        </Button>
       </form>
     );
   }
