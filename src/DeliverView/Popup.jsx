24f036570e04df93cd58ea949f5ceb92e3df7880
Anssi Mäkinen <anssi.a.makinen@gmail.com>
2017-11-30 15:48:52 +0200

    Add information to map popup

--- src/DeliverView/Popup.jsx
+++ src/DeliverView/Popup.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LatLonEllipsoidal as LatLon } from 'geodesy';
 import { Button } from 'react-bootstrap';
 
 export default class Popup extends React.Component {
@@ -7,9 +8,17 @@ export default class Popup extends React.Component {
   }
 
   render() {
+    const { delivery } = this.props;
+    const loc = new LatLon(delivery.location.lat, delivery.location.lng);
+    const dest = new LatLon(delivery.destination.lat, delivery.destination.lng);
+    const bearing = loc.initialBearingTo(dest);
+    const distance = loc.distanceTo(dest);
     return (
       <div className="mapPopup" style={{ transform: 'translate(-50%)', display: 'inline-block' }}>
-        TODO: DeliverMapPopup {this.props.delivery.name}
+        Name: {delivery.name}<br />
+        Bearing: {Number(bearing).toFixed(0)}°<br />
+        Distance: {Number(distance).toFixed(0)} m<br />
+        Deliver by: {delivery.deliverLatest.calendar()}
         <Button onClick={this.showDetails}>Show details</Button>
       </div>
     );
