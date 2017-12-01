a599521e40c2c257b36629dfce9dc1b0461209f6
Anssi Mäkinen <anssi.a.makinen@gmail.com>
2017-12-01 07:13:18 +0200

    Add top banner to SendView

--- src/SendView.jsx
+++ src/SendView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TopBanner from './util/TopBanner';
 
 export default class SendView extends React.Component {
   constructor(props) {
@@ -9,6 +10,7 @@ export default class SendView extends React.Component {
   render() {
     return (
       <div>
+        <TopBanner text="Send something" />
         TODO: SendView<br />
         <input
           type="button"
