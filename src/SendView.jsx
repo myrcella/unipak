c69404d80ca5ab4505a21f3fddc1bd005daece7d
myrcella <saga.nyren@gmail.com>
2017-12-03 14:21:11 +0200

    Sendview first iteration and some css stuff

--- src/SendView.jsx
+++ src/SendView.jsx
@@ -1,22 +1,72 @@
 import React from 'react';
+import { Checkbox, Button } from 'react-bootstrap';
+import FieldGroup from './util/FieldGroup';
 import TopBanner from './util/TopBanner';
 
 export default class SendView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+			fromAddress: '',
+			toAddress: '',
+			content: '',
+			date: '',
+			time: '',
+			offer: '',
+		};
+  }
+
+  handleChange = (event) => {
+    const input = event.target;
+    const key = input.name;
+    const value = input.type === 'checkbox' ? input.checked : input.value;
+
+    this.setState({
+      [key]: value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    // TODO Validation
+    // State object should match what setUserInfo expects
+    this.props.setUserInfo(this.state);
+    event.preventDefault();
   }
 
   render() {
     return (
       <div>
         <TopBanner text="Send something" />
-        TODO: SendView<br />
-        <input
-          type="button"
-          value="Back"
-          onClick={this.props.hideSendView}
+        <FieldGroup
+          name="fromAddress"
+          type="text"
+          label="From address"
+          help="Where should the package be picked up from"
+          value={this.state.fromAddress}
+          onChange={this.handleChange}
         />
+        <FieldGroup
+          name="toAddress"
+          type="text"
+          label="To address"
+          help="Where do you want it dropped off"
+          value={this.state.toAddress}
+          onChange={this.handleChange}
+        />
+        <FieldGroup
+          name="content"
+          type="text"
+          label="Content"
+          help="Describe the content (i.e. 'electronics')"
+          value={this.state.content}
+          onChange={this.handleChange}
+        />
+        <Button bsClass="btn btn-lg btnGr btnUni" type="submit" block>
+          Submit
+        </Button>
+        <Button bsClass="btn btn-lg btnOr btnUni" type="button" block>
+          Back
+        </Button>
       </div>);
   }
 }
